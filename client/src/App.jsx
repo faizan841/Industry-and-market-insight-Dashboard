424a56084fc6810eda15346b1926f5f877a0ef08
faizan841 <faizan200620@gmail.com>
2024-07-20 02:18:13 +0530

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Suspense, lazy } from "react";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { themeSettings } from "./theme";
-import Dashboard from "./scenes/dashboard/index";
-import Layout from "./scenes/layout/index";
-import Likelihood from "./scenes/Likelihood/Likelihood";
-import Country from "./scenes/country/Country";
-import Topics from "./scenes/topics/Topics";
-import Year from "./scenes/year/Year";
+
+const Dashboard = lazy(() => import("./scenes/dashboard/index"));
+const Likelihood = lazy(() => import("./scenes/Likelihood/Likelihood"));
+const Country = lazy(() => import("./scenes/country/Country"));
+const Topics = lazy(() => import("./scenes/topics/Topics"));
+const Year = lazy(() => import("./scenes/year/Year"));
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -22,11 +23,46 @@ const App = () => {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/likelihood" element={<Likelihood />} />
-              <Route path="/country" element={<Country />} />
-              <Route path="/topics" element={<Topics />} />
-              <Route path="/year" element={<Year />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Dashboard />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/likelihood"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Likelihood />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/country"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Country />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/topics"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Topics />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/year"
+                element={
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <Year />
+                  </Suspense>
+                }
+              />
             </Route>
           </Routes>
         </ThemeProvider>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: "./", // Tell Vite to load the.env file from the root of the project
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: "bundle-analysis.html",
+      template: "sunburst",
+    }),
+  ],
+  envDir: "./",
   envPrefix: "VITE_",
+  build: {
+    minify: "terser", // Use terser for minification
+    chunkSizeWarningLimit: 500, // Set the chunk size warning limit to 500KB
+    rollupOptions: {
+      input: "src/main.jsx",
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Separate vendor modules
+          }
+          if (id.includes("src/scenes/dashboard")) {
+            return "dashboard"; // Separate dashboard modules
+          }
+          if (id.includes("src/scenes/Likelihood")) {
+            return "likelihood"; // Separate likelihood modules
+          }
+          if (id.includes("src/scenes/country")) {
+            return "country"; // Separate country modules
+          }
+          if (id.includes("src/scenes/topics")) {
+            return "topics"; // Separate topics modules
+          }
+          if (id.includes("src/scenes/year")) {
+            return "year"; // Separate year modules
+          }
+        },
+      },
+    },
+  },
 });
